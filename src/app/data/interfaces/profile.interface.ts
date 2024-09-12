585d921067cebb32cab2b6ba3ed27efc3b53d709
Nastassia Hardzeenka <gordeenko.na@gmail.com>
2024-09-12 03:23:12 +0200

    first commit

---
+++ src/app/data/interfaces/profile.interface.ts
@@ -0,0 +1,12 @@
+export interface Profile {
+   id: number,
+   username: string,
+   avatarUrl: string | null,
+   subscribersAmount: number,
+   firstName: string,
+   lastName: string,
+   description: string,
+   isActive: boolean,
+   stack: string[],
+   city: string
+}
